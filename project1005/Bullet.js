b09e1a5cbb7af4aaeadffbf76fa0e54495afd7a5
ISPserver <tjdals080@naver.com>
2020-10-06 15:49:30 +0900

    JSON & 객체 &

--- project1005/Bullet.js
+++ project1005/Bullet.js
@@ -21,6 +21,26 @@ class Bullet{
   }
   tick(){ //총알의 물리량 변화
     this.y+=this.velY;
+
+    //총알이 한걸음 한걸음 진행할때마다 배열에 존재하는 모든 적군대상
+    //충돌했는지 여부 판단
+    //적군배열에서 요소삭제 splice(index,갯수), 화면에서도 삭제(removeChild)    
+    for(var i=0; i<2; i++){
+      for(var j=0; j<enemyArray.length; j++){
+        var result = collisionCheck(this.img, enemyArray[j].img);
+        if(result){
+          var img = enemyArray[j].img;
+          wrapper.removeChild(img);
+          var indexEnemy = enemyArray.indexOf(enemyArray[j].img); //현재 총알의 인스턴스가 배열의 몇번쨰에 들어있는지
+          enemyArray.splice(indexEnemy,indexEnemy+1);
+
+          //총알배열에서 요소삭제 splice(index,갯수), 화면에서도 삭제(removeChild)        
+          wrapper.removeChild(this.img);
+          var index = bulletArray.indexOf(this); //현재 총알의 인스턴스가 배열의 몇번쨰에 들어있는지
+          bulletArray.splice(index,index+1);
+        }
+      }
+    }
   }
 
   render(){
